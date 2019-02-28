aec3e54e81a3efdc1b4dc39694af5356270d8c67
mike muponda <mupondamike@gmail.com>
2019-02-27 21:20:59 -0500

    Backend->signup

---
+++ server/routes/api/signup.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+const loadUsers = async function() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb://lekka:lekka123@ds046377.mlab.com:46377/collegehub,
+    {
+      useNewUrlParser: true
+    }
+  )
+  return client.db('collegehub').collection('users')
+}
+
+router.get('/', async (req, res) => {
+  const users = await loadUsers()
+  res.send(await users.find({}).toArray())
+})
+
+router.post('/', async (req, res) => {
+  const users = await loadUsers()
+  await users.insertOne({
+    firstname: req.body.firstname,
+    middle_name: req.body.middle_name,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    password: req.body.password,
+    location: req.body.location,
+    houses_owned: req.body.houses_owned,
+    house_id: req.body.house_id,
+    house_no: req.body.house_no,
+    street: req.body.street,
+    surbub: req.body.surbub,
+    requests_made: req.body.requests_made,
+    createdAt: new Date()
+  })
+  res.status(201).send()
+})
+
+module.exports = router
