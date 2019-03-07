03409ec2d8e8ec16986527ef5ba8797782054e49
IamTinashe <tinashetp@gmail.com>
2019-03-07 19:16:39 +0300

    Sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,12 @@ const pkg = require('./package')
 
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const appRoutes = require('./routes.json')
+const axios = require('axios');
+
+//var allUsers = axios.get('https://www.lekkahub.com/users/allusers')
+var allUsers = appRoutes
+var routes = []
 
 let PWAIcons = [16, 120, 144, 152, 192, 384, 512].map(size => {
   return {
@@ -11,6 +17,13 @@ let PWAIcons = [16, 120, 144, 152, 192, 384, 512].map(size => {
   }
 })
 
+for (var i = 0; i < allUsers.length; i++) {
+  routeObject = {
+    'url': '/profile/' + allUsers[i].username,
+    'changefreq': 'daily'
+  }
+  routes.push(routeObject);
+}
 
 module.exports = {
   mode: 'universal',
@@ -42,27 +55,91 @@ module.exports = {
   },
 
   head: {
-    title: 'Collegehub',
-    meta: [
-      { charset: 'utf-8', title: 'Collegehub'},
-      { name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      { hid: 'description', name: 'description', content: 'Collegehub provides services that facilitate a smooth transition into college life and allow students to create the best experiences at college.'},
-      { hid: 'keywords', name: 'keywords', content: 'Collegehub, College, Accommodation, Rent a House, University, UZ, University of Zimbabwe, Harare, Lekkahub'},
+    meta: [{
+        charset: 'utf-8',
+        title: 'Collegehub'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Collegehub provides services that facilitate a smooth transition into college life and allow students to create the best experiences at college.'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'Collegehub, College, Accommodation, Rent a House, University, UZ, University of Zimbabwe, Harare, Lekkahub'
+      },
 
-      { hid: 'og:site_name', property: 'og:site_name', content: 'Collegehub'},
-      { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:title', property: 'og:title', content: 'Collegehub'},
-      { hid: 'og:description', property: 'og:description', content: 'Collegehub provides services that facilitate a smooth transition into college life and allow students to create the best experiences at college.'},
-      { hid: 'og:url', property: 'og:url', content: 'https://www.collegehub.co.zw'},
-      { hid: 'og:image', property: 'og:image', content: '/img/logo.png'},
-      { hid: 'og:locale', property: 'og:locale', content: 'en_GB'},
-      { hid: 'og:locale:alternate', property: 'og:locale:alternate', content: 'en_US'},
-      { hid: 'og:locale:alternate', property: 'og:locale:alternate', content: 'sn_ZW'},
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Collegehub'
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Collegehub'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Collegehub provides services that facilitate a smooth transition into college life and allow students to create the best experiences at college.'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://www.collegehub.co.zw'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/img/logo.png'
+      },
+      {
+        hid: 'og:locale',
+        property: 'og:locale',
+        content: 'en_GB'
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'en_US'
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'sn_ZW'
+      },
 
-      { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image'},
-      { hid: 'twitter:site', property: 'twitter:site', content: '@collegehub_zwe'},
-      { hid: 'twitter:creator', property: 'twitter:creator', content: '@lekkahub'},
-      { hid: 'twitter:image:alt', property: 'twitter:image:alt', content: 'Collegehub'},
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'twitter:site',
+        property: 'twitter:site',
+        content: '@collegehub_zwe'
+      },
+      {
+        hid: 'twitter:creator',
+        property: 'twitter:creator',
+        content: '@lekkahub'
+      },
+      {
+        hid: 'twitter:image:alt',
+        property: 'twitter:image:alt',
+        content: 'Collegehub'
+      },
     ],
     link: [{
         rel: 'icon',
@@ -86,11 +163,18 @@ module.exports = {
   ],
 
   //loading: '~/components/loading.vue',
-  loading: { color: '#da552f' },
+  loading: {
+    color: '#da552f'
+  },
 
-  plugins: [
-    {src: "~/plugins/burger-menu.js", ssr: false},
-    {src: "~/plugins/vue-cookie-law.js", ssr: false},
+  plugins: [{
+      src: "~/plugins/burger-menu.js",
+      ssr: false
+    },
+    {
+      src: "~/plugins/vue-cookie-law.js",
+      ssr: false
+    },
   ],
 
   serverMiddleware: [
@@ -118,18 +202,10 @@ module.exports = {
       '/secret',
       '/admin/**'
     ],
-    routes: [
-      '/',
-      {
-        url: '/',
-        changefreq: 'daily',
-        priority: 1,
-        lastmodISO: '2019-02-25T13:30:00.000Z'
-      }
-    ]
+    routes: routes
   },
-  
+
   axios: {},
 
-  build: {}
+  build: {},
 }
