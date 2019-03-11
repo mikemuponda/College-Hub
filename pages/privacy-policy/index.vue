26f18f2a0f0dcb522380c489cc90c49e5dd17611
IamTinashe <tinashetp@gmail.com>
2019-03-11 23:22:14 +0300

    NewPage/PrivacyPolicy

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div style="width: 100%; background: #eee;">
+    <div class="container" style="background: #eee; padding-top: 20px; padding-bottom: 20px;">
+      <div class="section" style="margin: 20px 0 20px 0; padding: 0 5% 0 5%; border-radius: 1px;">
+        <h1 class="heading-two">Welcome to our Privacy Policy</h1>
+        <h3 class="subheading-two">Your privacy is critically important to us.</h3>
+        <br>
+        <div class="row">
+          <div class="col-md-1"></div>
+          <div class="col-md-10 pp">
+            <p>
+              It is Collegehub’s policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to
+              <a
+                href="https://www.collegehub.co.zw"
+              >https://www.collegehub.co.zw</a> (hereinafter, "us", "we", or "https://www.collegehub.co.zw"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.
+            </p>
+            <p>This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the general rules and policies governing your use of our Website. Depending on your activities when visiting our Website, you may be required to agree to additional terms and conditions.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Website Visitors</h2>
+            <p>Like most website operators, Collegehub collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Collegehub’s purpose in collecting non-personally identifying information is to better understand how Collegehub’s visitors use its website. From time to time, Collegehub may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
+            <p>Collegehub also collects potentially personally-identifying information. Collegehub only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below.</p>
+
+            <h2
+              class="subheading-two"
+              style="text-align: left;"
+            >Gathering of Personally-Identifying Information</h2>
+            <p>Certain visitors to Collegehub’s websites choose to interact with Collegehub in ways that require Collegehub to gather personally-identifying information. The amount and type of information that Collegehub gathers depends on the nature of the interaction. For example, we ask visitors who sign up at https://www.collegehub.co.zw to provide a username and email address.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Security</h2>
+            <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Advertisements</h2>
+            <p>Ads appearing on our website may be delivered to users by advertising partners, who may set cookies. These cookies allow the ad server to recognize your computer each time they send you an online advertisement to compile information about you or others who use your computer. This information allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most interest to you. This Privacy Policy covers the use of cookies by Collegehub and does not cover the use of cookies by any advertisers.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Links To External Sites</h2>
+            <p>Our Service may contain links to external sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy and terms and conditions of every site you visit.</p>
+            <p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites, products or services.</p>
+
+            <h2
+              class="subheading-two"
+              style="text-align: left;"
+            >Protection of Certain Personally-Identifying Information</h2>
+            <p>Collegehub discloses potentially personally-identifying and personally-identifying information only to those of its employees, contractors and affiliated organizations that (i) need to know that information in order to process it on Collegehub’s behalf or to provide services available at Collegehub’s website, and (ii) that have agreed not to disclose it to others. Some of those employees, contractors and affiliated organizations may be located outside of your home country; by using Collegehub’s website, you consent to the transfer of such information to them. Collegehub will not rent or sell potentially personally-identifying and personally-identifying information to anyone. Other than to its employees, contractors and affiliated organizations, as described above, Collegehub discloses potentially personally-identifying and personally-identifying information only in response to a subpoena, court order or other governmental request, or when Collegehub believes in good faith that disclosure is reasonably necessary to protect the property or rights of Collegehub, third parties or the public at large.</p>
+            <p>If you are a registered user of https://www.collegehub.co.zw and have supplied your email address, Collegehub may occasionally send you an email to tell you about new features, solicit your feedback, or just keep you up to date with what’s going on with Collegehub and our products. We primarily use our blog to communicate this type of information, so we expect to keep this type of email to a minimum. If you send us a request (for example via a support email or via one of our feedback mechanisms), we reserve the right to publish it in order to help us clarify or respond to your request or to help us support other users. Collegehub takes all measures reasonably necessary to protect against the unauthorized access, use, alteration or destruction of potentially personally-identifying and personally-identifying information.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Aggregated Statistics</h2>
+            <p>Collegehub may collect statistics about the behavior of visitors to its website. Collegehub may display this information publicly or provide it to others. However, Collegehub does not disclose your personally-identifying information.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Cookies</h2>
+            <p>To enrich and perfect your online experience, Collegehub uses "Cookies", similar technologies and services provided by others to display personalized content, appropriate advertising and store your preferences on your computer.</p>
+            <p>A cookie is a string of information that a website stores on a visitor’s computer, and that the visitor’s browser provides to the website each time the visitor returns. Collegehub uses cookies to help Collegehub identify and track visitors, their usage of https://www.collegehub.co.zw, and their website access preferences. Collegehub visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Collegehub’s websites, with the drawback that certain features of Collegehub’s websites may not function properly without the aid of cookies.</p>
+            <p>By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to Collegehub's use of cookies.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">E-commerce</h2>
+            <p>Those who engage in transactions with Collegehub – by purchasing Collegehub's services or products, are asked to provide additional information, including as necessary the personal and financial information required to process those transactions. In each case, Collegehub collects such information only insofar as is necessary or appropriate to fulfill the purpose of the visitor’s interaction with Collegehub. Collegehub does not disclose personally-identifying information other than as described below. And visitors can always refuse to supply personally-identifying information, with the caveat that it may prevent them from engaging in certain website-related activities.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Business Transfers</h2>
+            <p>If Collegehub, or substantially all of its assets, were acquired, or in the unlikely event that Collegehub goes out of business or enters bankruptcy, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any acquirer of Collegehub may continue to use your personal information as set forth in this policy.</p>
+
+            <h2 class="subheading-two" style="text-align: left;">Privacy Policy Changes</h2>
+            <p>Although most changes are likely to be minor, Collegehub may change its Privacy Policy from time to time, and in Collegehub’s sole discretion. Collegehub encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.</p>
+
+            <h2 class="subheading-two" style="text-align: left;"></h2>
+            <p></p>
+
+            <h2 class="subheading-two" style="text-align: left;">Credit & Contact Information</h2>
+            <p>
+              If you have any questions about this Privacy Policy, please contact us via
+              <a
+                href="mailto:hello@collegehub.co.zw"
+              >hello@collegehub.co.zw</a> or
+              <a href="tel:0778011367">0778011367</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.pp p {
+	text-align: justify;
+	color: #707070;
+	font-size: 14px;
+}
+</style>
+
