784f531d0da2c48f72d16ca58630948b0be3fbda
IamTinashe <tinashetp@gmail.com>
2019-03-13 03:48:43 +0300

    Page/About

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,155 +1,150 @@
 <template>
   <div style="width:100%">
     <!--snippet from bootsnipp.com statrs after this-->
-
-    <div style="width: 100%;">
-        <div class="aboutus-section">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-8 col-sm-12 col-xs-12">
-                <div class="aboutus item-box" style="border-radius:4px;padding:10px;text-align:center">
-                      <h2 class="aboutus-title">About Us</h2>
-                      <p class="aboutus-text">
-                      College hub is a local online company whose core business is providing services to college students,
-                      enrolled at local Zimbabwean universities.College hub was found in 2019 and set up by a group of young
-                      people who are passionate about bringing transformation to our nation.Our aim is to make college lfe
-                      easy and a smooth ride for students by bringing the world to them.We accomplish this objective by combining
-                      all the services students require on one site.
-
-                      Why us:
-
-
-
+    <section id="about" class="pb-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-8 col-xs-8">
+            <div class="item-box">
+              <h2 class="heading-two">About Us</h2>
+            </div>
+            <div class="item-box">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2 class="subheading-three">We are Collegehub!</h2>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <p class="about-us-p">
+                        We provide awesome services to simplify college life. We are passionate about
+                        making university experiences an easy ride.
                       </p>
-                      <p class="aboutus-text">
-                      Our main service is assisting students in accessing external accommodation without any hustle.
-                      We are bringing an effective solution to the current way of physically looking for a place to stay.
-                      We have engaged many house owners and our site enables students to navigate through different housing
-                      options available and make a reservation.After payment of a fee the application is processed and the
-                      student can secure their room.
-                       </p>
-                       <a href="#" class="aboutus-more" style="">read more</a>
-                   </div>
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
-
-    <!--it also end here-->
+                      <p class="about-us-p">We are based in Zimbabwe and we do amazing things.</p>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="about-img">
+                        <img src="/img/logo.png">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2 class="subheading-three">What drives us</h2>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="about-img">
+                        <img src="/img/logo.png">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <p class="about-us-p">
+                        We provide awesome services to simplify college life. We are passionate about
+                        making university experiences an easy ride.
+                      </p>
+                      <p class="about-us-p">We are based in Zimbabwe and we do amazing things.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2 class="subheading-three">Our Values</h2>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <p class="about-us-p">
+                        Our Values are this and that
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2 class="subheading-three">A short Background</h2>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <p class="about-us-p">
+                        Founded in 2019 by a group of young people with the aim to ease life for
+                        college students, Collegehub seeks to bridge students to various services
+                        that students need.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-4">
+            <div class="item-box"><h2 class="subheading-three">ads and other stuff to come here</h2></div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-<style>
-    .item-box {
-    background-color: #fff;
-    border-radius: 1px;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    -webkit-box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.5);
-    -moz-box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.5);
-    box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.5);
+<script>
+export default {
+  head() {
+    return {
+      title: 'Collegehub | About Us'
     }
-    .aboutus-section {
-    padding: 90px 0;
-    }
-    .aboutus-title {
-    font-size: 30px;
-    letter-spacing: 0;
-    line-height: 32px;
-    margin: 0 0 39px;
-    padding: 0 0 11px;
-    position: relative;
-    text-transform: uppercase;
-    color:#707070;
-    }
-
-    .aboutus-text {
-    color: #707070;
-    font-size: 17px;
-    line-height: 22px;
-    margin: 0 0 35px;
-
-    }
-
-    a:hover, a:active {
-    color: #ffb901;
-    text-decoration: none;
-    outline: 0;
-    }
-    .aboutus-more {
-    border: 1px solid #fdb801;
-    border-radius: 25px;
-    color: #707070;
-    display: inline-block;
-    font-size: 14px;
-    font-weight: 700;
-    letter-spacing: 0;
-    padding: 7px 20px;
-    text-transform: uppercase;
-    }
-    .feature .feature-box .iconset {
-    background: #fff none repeat scroll 0 0;
-    float: left;
-    position: relative;
-    width: 18%;
-    }
-    .feature .feature-box .iconset::after {
-    background: #fdb801 none repeat scroll 0 0;
-    content: "";
-    height: 150%;
-    left: 43%;
-    position: absolute;
-    top: 100%;
-    width: 1px;
-    }
-
-    .feature .feature-box .feature-content h4 {
-    color: #707070;
-    font-size: 17px;
-    letter-spacing: 0;
-    line-height: 22px;
-    margin: 0 0 5px;
-    }
-
-
-    .feature .feature-box .feature-content {
-    float: left;
-    padding-left: 28px;
-    width: 78%;
-    }
-    .feature .feature-box .feature-content h4 {
-    color: #707070;
-    font-size: 18px;
-    letter-spacing: 0;
-    line-height: 22px;
-    margin: 0 0 5px;
-    }
-    .feature .feature-box .feature-content p {
-    color: #707070;
-    font-size: 13px;
-    line-height: 22px;
-    }
-    .icon {
-    color : ##707070;
-    padding:0px;
-    font-size:40px;
-    border: 1px solid #fdb801;
-    border-radius: 100px;
-    color: #707070;
-    font-size: 28px;
-    height: 70px;
-    line-height: 70px;
-    text-align: center;
-    width: 70px;
-    }
-
-  </style>
-
-  <script>
-      export default{
-      methods:{
-
-      }
-      }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+    })
+    this.$nextTick(() => {
+      setTimeout(() => this.$nuxt.$loading.finish(), 400)
+    })
+  }
+}
 </script>
+
+<style>
+.item-box {
+  background-color: #fff;
+  border-radius: 1px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  -webkit-box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.5);
+  -moz-box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.5);
+  box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.5);
+  margin-top: 10px;
+}
+
+section {
+  padding: 60px 0;
+}
+
+.about-us-p {
+  padding: 20px;
+  text-align: justify;
+}
+
+.about-img {
+  width: 100%;
+}
+
+.about-img img {
+  width: 100%;
+}
+</style>
+
