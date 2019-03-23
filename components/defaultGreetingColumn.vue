effbc7b7d1dd8fa0f9d4c5a93f16a9b02406264c
IamTinashe <tinashetp@gmail.com>
2019-03-24 00:43:59 +0300

    Restructure

--- components/defaultGreetingColumn.vue
+++ components/defaultGreetingColumn.vue
@@ -24,25 +24,144 @@
         </div>
         <br>
         {{userProfile.firstname}} {{userProfile.lastname}}
+        <br>
+        <br>
+        <small>Your profile is {{counter}}% complete</small>
+        <b-progress :value="counter" :max="max" show-progress animated/>
       </div>
     </NuxtLink>
+
+    <div class="row nopadding mobileHide" style="width: 100%;">
+      <div class="col-md-12 nopadding">
+        <div
+          class="item shadow cf"
+          style="width: 100%; background-color: #fff; height: 20px; border-bottom: 0.5px solid #aaa;"
+        >
+          <h2 style="font-size: 11px; text-align: center; padding-top: 5px;">Activity Feed</h2>
+        </div>
+      </div>
+    </div>
+    <!-- Feed Item Start -->
+    <div
+      style="background-color: #fff; border-radius: 2px;"
+      class="item white shadow cf mobileHide"
+      v-for="(activity, index) in activities.slice().reverse()"
+      :key="index"
+    >
+      <NuxtLink
+        :to="{path: '/profile/' + activity.user.username}"
+        :title="activity.user.firstname + ' ' + activity.user.lastname"
+        style="color: #606060;"
+      >
+        <div class="row nopadding">
+          <div class="activity-box col-12 col-persist gutter-h-10">
+            <div class="profile-image nopadding" style="float: left; width: 30%;">
+              <div
+                v-if="activity.user.profileImage"
+                class="ratio img-responsive img-circle"
+                :title="activity.user.firstname + ' ' + activity.user.lastname"
+                :alt="activity.user.firstname + ' ' + activity.user.lastname"
+                v-bind:style="{backgroundImage: 'url(' + activity.user.profileImage.path + ')'}"
+              ></div>
+              <div
+                v-else
+                class="ratio img-responsive img-circle"
+                style="background-image: url(/profileImages/user.png);"
+                :title="activity.user.firstname + ' ' + activity.user.lastname"
+                :alt="activity.user.firstname + ' ' + activity.user.lastname"
+              ></div>
+            </div>
+            <div style="float: left; width: 70%; text-align: center; margin-top: 10px;">
+              <span style="font-size:10px;">
+                <strong>{{ activity.user.username }}</strong><br>
+                <p class="nopadding">{{activity.activity}}</p>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="row padding">
+          <div
+            class="activity-box col-12 col-persist gutter-h-10 padding-15"
+            style="text-align: right;"
+          >
+            <span style="font-size:9px; color: #606060; padding-right: 5px;">{{timer()}}</span>
+          </div>
+        </div>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client'
 export default {
   data() {
     return {
-      userProfile: ''
+      userProfile: '',
+      counter: 0,
+      max: 100,
+      activity: '',
+      activities: [],
+      time: '',
+      socket: io('lekkahub.com')
+    }
+  },
+  methods: {
+    shareActivity() {
+      this.socket.emit('ACTIVITY_FEED', {
+        user: this.user.username,
+        activity: this.activity
+      })
+      this.activity = ''
+    },
+    timer() {
+      var d = new Date()
+      return d.toLocaleTimeString()
     }
   },
   async created() {
     try {
-      this.userProfile = await this.$store.dispatch('getProfile', {id: this.$store.state.authUser.user.username})
+      this.userProfile = await this.$store.dispatch('getProfile', {
+        id: this.$store.state.authUser.user.username
+      })
       this.userProfile = this.userProfile.data.user
+      var i = 0,
+        j = 0
+      for (let index in this.userProfile) {
+        j++
+        if (this.userProfile.hasOwnProperty(index)) {
+          var value = this.userProfile[index]
+          if (value == '' || value == null) {
+            i++
+          }
+        }
+      }
+      this.counter = Math.round((i / j) * this.max)
+
+      this.socket.emit('ACTIVITY_FEED', {
+        user: this.userProfile,
+        activity: 'is logged in'
+      })
+      this.socket.on('ACTIVITY', data => {
+        this.activities.push(data)
+        if (this.userProfile.username != data.user.username) {
+          var audio = new Audio('/notifications/open-ended.mp3')
+          audio.play()
+        }
+      })
+      this.activity = ''
     } catch (e) {
       this.error = e.message
     }
   }
 }
 </script>
+
+<style>
+@media (max-width: 767px) {
+  .mobileHide{
+    display: none;
+  }
+}
+</style>
+
--- components/userDashboard.vue
+++ components/userDashboard.vue
@@ -4,34 +4,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-2 dashboard-greeting-display">
-            <NuxtLink
-              :to="{path: '/profile/' + $store.state.authUser.user.username}"
-              title="Profile"
-              style="color: #606060;"
-            >
-              <div class="user-profile">
-                <div class="profile-image">
-                  <div
-                    v-if="userProfile.profileImage"
-                    class="ratio img-responsive img-circle"
-                    :title="userProfile.firstname + ' ' + userProfile.lastname"
-                    :alt="userProfile.firstname + ' ' + userProfile.lastname"
-                    v-bind:style="{
-                      backgroundImage: 'url(' + userProfile.profileImage.path + ')'
-                    }"
-                  ></div>
-                  <div
-                    v-else
-                    class="ratio img-responsive img-circle"
-                    style="background-image: url(/profileImages/user.png);"
-                    :title="userProfile.firstname + ' ' + userProfile.lastname"
-                    :alt="userProfile.firstname + ' ' + userProfile.lastname"
-                  ></div>
-                </div>
-                <br>
-                {{$store.state.authUser.user.firstname}} {{$store.state.authUser.user.lastname}}
-              </div>
-            </NuxtLink>
+            <greetingColumn/>
           </div>
           <div class="col-md-10">
             <div class="row">
@@ -42,7 +15,7 @@
                       <img
                         class="img-fluid mb-3 mb-md-0 feature-img"
                         src="/img/collegehub-from-mobile.jpeg"
-                        alt ="List your house"
+                        alt="List your house"
                       >
                     </div>
                     <div style="width: 60%; float: left;">
@@ -54,13 +27,13 @@
               </div>
 
               <div class="col-md-4 dashboard-greeting-display">
-                <NuxtLink :to="'/taxi/find'" title= "Get a Taxi" style="color: #606060;">
+                <NuxtLink :to="'/taxi/find'" title="Get a Taxi" style="color: #606060;">
                   <div class="row nopadding feature">
                     <div class="nopadding" style="width: 40%; float: left;">
                       <img
                         class="img-fluid mb-3 mb-md-0 feature-img"
                         src="/img/collegehub-taxi.jpeg"
-                        alt = "Get a Taxi"
+                        alt="Get a Taxi"
                       >
                     </div>
                     <div style="width: 60%; float: left;">
@@ -151,161 +124,118 @@
                 </NuxtLink>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div class="container" style="margin-top: 50px;">
-        <div class="row">
-          <div class="col-md-2">
-            <div class="row nopadding" style="width: 100%;">
-              <div class="col-md-12 nopadding">
-                <div
-                  class="item shadow cf"
-                  style="width: 100%; background-color: #ddd; height: 20px; border-bottom: 0.5px solid #aaa;"
-                >
-                  <h2 style="font-size: 11px; text-align: center; padding-top: 5px;">Activity Feed</h2>
-                </div>
-              </div>
-            </div>
-            <!-- Feed Item Start -->
-            <div
-              style="margin-top: 20px;"
-              class="item white shadow cf"
-              v-for="(activity, index) in activities.slice().reverse()"
-              :key="index"
-            >
-            <NuxtLink
-              :to="{path: '/profile/' + activity.user}"
-              :title="activity.user"
-              style="color: #606060;"
-            >
-              <div class="row padding">
-                <div class="activity-box col-12 col-persist gutter-h-10 padding-15">
-                  <span style="font-size:12px; padding-left: 5px;">
-                    <strong>{{ activity.user }}</strong>
-                    {{activity.activity}}
-                  </span>
-                </div>
-              </div>
-              <div class="row padding">
-                <div
-                  class="activity-box col-12 col-persist gutter-h-10 padding-15"
-                  style="text-align: right;"
-                >
-                  <span style="font-size:10px; color: #606060; padding-right: 5px;">{{timer()}}</span>
-                </div>
-              </div>
-              </NuxtLink>
-            </div>
-          </div>
-
-          <div class="col-md-7">
-            <div class="row nopadding default-user-panel">
-              <div
-                class="row nopadding alert alert-success"
-                v-if="!$store.state.authUser.user.isConfirmed"
-              >
-                <div class="col-md-12">
-                  <h2 class="section-title">Hi, {{$store.state.authUser.user.firstname}}</h2>
-                  <p style="font-size: 13px;">
-                    Please note that your account has not been confirmed yet therefore you will not be able
-                    to use this service. We sent an email with a confirmation link to: {{$store.state.authUser.user.email}}.
-                    <br>Follow the link in the email, otherwise contact support.
-                  </p>
-                </div>
-              </div>
-
-              <div style="width: 100%;" v-else>
-                <div class="row nopadding" style="width: 100%;">
-                  <div class="col-md-12 inner-title-sec">
-                    <h2>Rental Requests</h2>
-                  </div>
-                </div>
-
-                <div class="row nopadding feed">
-                  <div class="col-md-12">
-                    <div class="rental-image-div" style="width: 20%; float: left;">
-                      <div
-                        class="ratio img-responsive img-circle"
-                        style="background-image: url(/houses/collegehub-alexandra.jpeg);"
-                      ></div>
+            <div class="row" style="margin-top: 20px;">
+              <div class="col-md-8">
+                <div class="row nopadding default-user-panel">
+                  <div
+                    class="row nopadding alert alert-success"
+                    v-if="!$store.state.authUser.user.isConfirmed"
+                  >
+                    <div class="col-md-12">
+                      <h2 class="section-title">Hi, {{$store.state.authUser.user.firstname}}</h2>
+                      <p style="font-size: 13px;">
+                        Please note that your account has not been confirmed yet therefore you will not be able
+                        to use this service. We sent an email with a confirmation link to: {{$store.state.authUser.user.email}}.
+                        <br>Follow the link in the email, otherwise contact support.
+                      </p>
                     </div>
-                    <div class="-div" style="width: 80%; float: left;">
-                      <div class="row">
-                        <div class="col-md-8">
-                          <h3 class="section-subtitle">Avondale, Harare</h3>
-                          <p
-                            class="section-small-text"
-                          >King George Street &#8811; Avondale &#8811; Harare</p>
-                          <div class="section-amenities">
-                            <div style="width: 50%; float: left;">
-                              <p class="section-small-text">
-                                <i class="fas fa-graduation-cap"></i>UZ (10 minutes)
-                              </p>
-                              <p class="section-small-text">
-                                <i class="fas fa-wifi"></i>Wifi
-                              </p>
-                            </div>
-                            <div style="width: 50%; float: left;">
-                              <p class="section-small-text">
-                                <i class="fas fa-bed"></i>
-                                2 Bedrooms
-                              </p>
-                              <p class="section-small-text">
-                                <i class="fas fa-couch"></i>
-                                Furnished
-                              </p>
-                            </div>
-                          </div>
+                  </div>
+
+                  <div style="width: 100%;" v-else>
+                    <div class="row nopadding" style="width: 100%;">
+                      <div class="col-md-12 inner-title-sec">
+                        <h2>Rental Requests</h2>
+                      </div>
+                    </div>
+
+                    <div class="row nopadding feed">
+                      <div class="col-md-12">
+                        <div class="rental-image-div" style="width: 20%; float: left;">
+                          <div
+                            class="ratio img-responsive img-circle"
+                            style="background-image: url(/houses/collegehub-alexandra.jpeg);"
+                          ></div>
                         </div>
-                        <div class="col-md-4">
-                          <div class="rental-alert">
-                            <h2 class="success">Approved</h2>
+                        <div class="-div" style="width: 80%; float: left;">
+                          <div class="row">
+                            <div class="col-md-8">
+                              <h3 class="section-subtitle">Avondale, Harare</h3>
+                              <p
+                                class="section-small-text"
+                              >King George Street &#8811; Avondale &#8811; Harare</p>
+                              <div class="section-amenities">
+                                <div style="width: 50%; float: left;">
+                                  <p class="section-small-text">
+                                    <i class="fas fa-graduation-cap"></i>UZ (10 minutes)
+                                  </p>
+                                  <p class="section-small-text">
+                                    <i class="fas fa-wifi"></i>Wifi
+                                  </p>
+                                </div>
+                                <div style="width: 50%; float: left;">
+                                  <p class="section-small-text">
+                                    <i class="fas fa-bed"></i>
+                                    2 Bedrooms
+                                  </p>
+                                  <p class="section-small-text">
+                                    <i class="fas fa-couch"></i>
+                                    Furnished
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-md-4">
+                              <div class="rental-alert">
+                                <h2 class="success">Approved</h2>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
 
-                <div class="row nopadding feed">
-                  <div class="col-md-12">
-                    <div class="rental-image-div" style="width: 20%; float: left;">
-                      <div
-                        class="ratio img-responsive img-circle"
-                        style="background-image: url(/houses/house-four.jpeg);"
-                      ></div>
-                    </div>
-                    <div class="-div" style="width: 80%; float: left;">
-                      <div class="row">
-                        <div class="col-md-8">
-                          <h3 class="section-subtitle">Avenues, Harare</h3>
-                          <p class="section-small-text">Baines Street &#8811; Avenues &#8811; Harare</p>
-                          <div class="section-amenities">
-                            <div style="width: 50%; float: left;">
-                              <p class="section-small-text">
-                                <i class="fas fa-graduation-cap"></i>UZ (10 minutes)
-                              </p>
-                              <p class="section-small-text">
-                                <i class="fas fa-wifi"></i>Wifi
-                              </p>
-                            </div>
-                            <div style="width: 50%; float: left;">
-                              <p class="section-small-text">
-                                <i class="fas fa-bed"></i>
-                                1 Bedroom
-                              </p>
-                              <p class="section-small-text">
-                                <i class="fas fa-couch"></i>
-                                Furnished
-                              </p>
-                            </div>
-                          </div>
+                    <div class="row nopadding feed">
+                      <div class="col-md-12">
+                        <div class="rental-image-div" style="width: 20%; float: left;">
+                          <div
+                            class="ratio img-responsive img-circle"
+                            style="background-image: url(/houses/house-four.jpeg);"
+                          ></div>
                         </div>
-                        <div class="col-md-4">
-                          <div class="rental-alert">
-                            <h2 class="danger">Disapproved</h2>
+                        <div class="-div" style="width: 80%; float: left;">
+                          <div class="row">
+                            <div class="col-md-8">
+                              <h3 class="section-subtitle">Avenues, Harare</h3>
+                              <p
+                                class="section-small-text"
+                              >Baines Street &#8811; Avenues &#8811; Harare</p>
+                              <div class="section-amenities">
+                                <div style="width: 50%; float: left;">
+                                  <p class="section-small-text">
+                                    <i class="fas fa-graduation-cap"></i>UZ (10 minutes)
+                                  </p>
+                                  <p class="section-small-text">
+                                    <i class="fas fa-wifi"></i>Wifi
+                                  </p>
+                                </div>
+                                <div style="width: 50%; float: left;">
+                                  <p class="section-small-text">
+                                    <i class="fas fa-bed"></i>
+                                    1 Bedroom
+                                  </p>
+                                  <p class="section-small-text">
+                                    <i class="fas fa-couch"></i>
+                                    Furnished
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-md-4">
+                              <div class="rental-alert">
+                                <h2 class="danger">Disapproved</h2>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -313,9 +243,9 @@
                   </div>
                 </div>
               </div>
+              <div class="col-md-4">Ads and Suggestions will come here</div>
             </div>
           </div>
-          <div class="col-md-3">Ads and Suggestions will come here</div>
         </div>
       </div>
     </div>
@@ -323,29 +253,15 @@
 </template>
 
 <script>
-import io from 'socket.io-client'
+import greetingColumn from '@/components/defaultGreetingColumn'
 export default {
+  components: {
+    greetingColumn: greetingColumn
+  },
   data() {
     return {
       user: '',
-      userProfile: '',
-      activity: '',
-      activities: [],
-      time: '',
-      socket: io('localhost:8080')
-    }
-  },
-  methods: {
-    shareActivity() {
-      this.socket.emit('ACTIVITY_FEED', {
-        user: this.user.username,
-        activity: this.activity
-      })
-      this.activity = ''
-    },
-    timer() {
-      var d = new Date()
-      return d.toLocaleTimeString()
+      userProfile: ''
     }
   },
   async created() {
@@ -357,18 +273,6 @@ export default {
     } catch (e) {
       this.error = e.message
     }
-    this.socket.emit('ACTIVITY_FEED', {
-      user: this.userProfile.username,
-      activity: 'has logged in'
-    })
-    this.socket.on('ACTIVITY', data => {
-      this.activities.push(data)
-      if (this.userProfile.username != data.user) {
-        var audio = new Audio('/notifications/open-ended.mp3')
-        audio.play()
-      }
-    })
-    this.activity = ''
   }
 }
 </script>
