812a9e1e913d28b2b3fe2ad3659cda381af748dd
IamTinashe <tinashetp@gmail.com>
2019-03-28 00:17:36 +0300

    ReAdd/Socket

--- server/index.js
+++ server/index.js
@@ -34,10 +34,10 @@ async function start() {
 
   // Listen the server
   const server = app.listen(port, host)
- // const io = require('socket.io')(server);
+  const io = require('socket.io')(server);
 
-  //io.on('connection', function(socket) { socket.on('SEND_MESSAGE', function(data) { io.emit('MESSAGE', data)});})
-  //io.on('connection', function(socket) { socket.on('ACTIVITY_FEED', function(data) { io.emit('ACTIVITY', data)});})
+  io.on('connection', function(socket) { socket.on('SEND_MESSAGE', function(data) { io.emit('MESSAGE', data)});})
+  io.on('connection', function(socket) { socket.on('ACTIVITY_FEED', function(data) { io.emit('ACTIVITY', data)});})
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
