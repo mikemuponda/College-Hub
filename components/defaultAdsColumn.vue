dbfb62d68ab5eba61432622dbbf8bb3ef0759540
IamTinashe <tinashetp@gmail.com>
2019-04-27 22:48:18 +0300

    G-Ads/Height

--- components/defaultAdsColumn.vue
+++ components/defaultAdsColumn.vue
@@ -1,8 +1,8 @@
 <template>
 	<div style="width: 100%;">
 		<div class="item-box" style="margin-top: 5px;">
-			<div style="max-height: 400px; width: 100%;">
-				<adsbygoogle/>
+			<div style="height: 500px; width: 100%;">
+				<adsbygoogle style="display:inline-block; height: 500px; width: 100%;"/>
 			</div>
 		</div>
 	</div>
