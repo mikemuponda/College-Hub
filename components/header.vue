c628f720ce9122404f56b185141146862dc07e7b
IamTinashe <tinashetp@gmail.com>
2019-05-03 22:25:14 +0300

    Nav/CSSFix

--- components/header.vue
+++ components/header.vue
@@ -229,7 +229,7 @@
   transition: all .3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  display: none;
 }
 .slide-fade-enter, .slide-fade-leave-to
 /* .slide-fade-leave-active below version 2.1.8 */ {
