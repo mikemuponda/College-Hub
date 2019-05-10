1d3690f2ce32314825ac3813c1e00dad385dbd42
ray <ray@health263.systems>
2019-05-10 15:50:49 +0200

    pagination

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -43,12 +43,12 @@
                       <li class="page-item">
                         <a class="page-link" href="#">1</a>
                       </li>
-                      <li class="page-item">
+                      <!-- <li class="page-item">
                         <a class="page-link" href="#">2</a>
                       </li>
                       <li class="page-item">
                         <a class="page-link" href="#">3</a>
-                      </li>
+                      </li> -->
                       <li class="page-item">
                         <a class="page-link" href="#">Next</a>
                       </li>
