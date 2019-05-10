3aa1b449456d2bc16e0adc3c1d94021310e82eda
ray <ray@health263.systems>
2019-05-10 13:14:33 +0200

    article text

--- pages/blog/article.vue/index.vue
+++ pages/blog/article.vue/index.vue
@@ -3,70 +3,12 @@
     <div class="container">
       <div style="margin-top: 50px; width: 100%; margin-bottom: 20px;">
         <div class="row">
-          <div class="col-md-6" style="text-align: center; margin-top: 10%; margin-bottom: 25%;">
-            <h1 class="title">Collegehub</h1>
-            <h2 class="subtitle">The premier service for university students</h2>
-          </div>
-          <div class="col-md-6">
-            <div class="col-md-10 login-card">
-              <form @submit.prevent="login" autocomplete="off">
-                <div class="col-lg-12 logo-capsul">
-                  <h2 class="heading">Login</h2>
-                </div>
-                <div v-if="formError" class="col-lg-12 logo-capsul">
-                  <p class="error">
-                    <i>{{ formError }}</i>
-                  </p>
-                </div>
-                <br>
-                <div style="clear:both;"></div>
-                <div class="group">
-                  <input
-                    v-model="formEmail"
-                    type="email"
-                    name="email"
-                    autocomplete="off"
-                    id="email"
-                    required
-                  >
-                  <span class="highlight"></span>
-                  <span class="bar"></span>
-                  <label for="email">
-                    <i class="fa fa-envelope input-ikon"></i>
-                    <span class="span-input">Email</span>
-                  </label>
-                </div>
-                <div class="group">
-                  <input
-                    v-model="formPassword"
-                    type="password"
-                    name="password"
-                    autocomplete="off"
-                    id="password"
-                    required
-                  >
-                  <span class="highlight"></span>
-                  <span class="bar"></span>
-                  <label for="password">
-                    <i class="fa fa-lock input-ikon"></i>
-                    <span class="span-input">Password</span>
-                  </label>
-                </div>
-                <button type="submit" class="login-button">Login</button>
-                <div class="forgot-and-create tab-menu">
-                  <NuxtLink
-                    :to="'/forgot-password'"
-                    class="forgot_password_link"
-                    title="Forgot Password"
-                  >Forgot you password?</NuxtLink>
-                  <br>
-                  <br>
-                  <NuxtLink :to="'/create-account'" title="Create Account">
-                    <button class="create-account" style="font-size: 15px;">Create Account</button>
-                  </NuxtLink>
-                </div>
-              </form>
-            </div>
+          <div class="col-md-12" style="text-align: center; margin-top: 13%; margin-bottom: 15%;">
+            <h3
+              class="title"
+            >“Design creates culture, culture shapes values and values determine the future” Robert L Peters</h3>
+
+            <h4 class="subtitle">Posted on 09 May 2019</h4>
           </div>
         </div>
       </div>
@@ -75,7 +17,36 @@
     <div style="width: 100%;">
       <div class="row">
         <div class="col-md-12 nopadding">
-          <homeDefault/>
+          <div style="width: 100%">
+            <div class="section">
+              <div class="container">
+                <div class="row nopadding">
+                  <div class="col-md-12" style="text-align: center;">
+                    <!-- <h5 class="section-title">“Design creates culture, culture shapes values and values determine the future” Robert L Peters</h5>
+                    <h6 class="section-subtitle-grey">A 10 min read</h6>-->
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-2 mt-2"></div>
+                  <div class="col-md-8 mt-2">
+                    <p
+                      class="article-text"
+                    >Welcome to Collegehub Zimbabwe an online service for your daily convenience, from accommodation, taxi, restaurants and buying and selling we got you covered. In this blog I’m going to take you (Hubbers) through a history of how this company was started and also share with you our culture and goals. Before we dive deep into it, allow me to shift your attention to that opening quote by Robert L Peters. That statement fits well into our system at college hub. The website was designed in a unique way to offer the best user experience and to cater for the major concerns of our clients.</p>
+                    <p class="article-text">At College hub we value team work, we believe in our uniqueness and that we are unlimited in our potential if we combine our strengths and tackle a problem. We value greatly the idea of team work and we hope to see it cascade down to our users and partners. We appreciate diversity at Collegehub and we provide an environment where our colleagues, partners and clients can fully express their true self and flourish. Therefore at Collegehub we hope to share our values of diversity, unity, teamwork with our clients and together create a future full of endless possibilities</p>
+
+                    <p
+                      class="article-text"
+                    >College hub was found in December 2018 and what drove our founders to creating this platform was the struggle, college students had to go through in order to secure external accommodation. We saw an very challenging situation for students and came up with a plan to tackle this problem for the student and that gave birth to collegehub.During the crafting of this intervention method we realized that students also struggled with other things, communication between different faculties, buying and selling wasn’t easy and efficient on campus, finding a taxi, among other things and we made it our goal to bring these services on one site.</p>
+
+                    <p
+                      class="article-text"
+                    >Our goal is to remove the burden from the student and carry it on their behalf. We appreciate how time is limited and we want to help the students access these services in an easy and efficient manner. Welcome once again Hubbers, we promise you a smooth journey with us and we have your back all the way.</p>
+                  </div>
+                     <div class="col-md-2 mt-2"></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -83,7 +54,13 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
+<style scoped>
+.article-text {
+  font-size: 23px;
+  line-height: 1.5;
+  margin: 30px 0;
+  font-family: "Lora", "Times New Roman", serif;
+}
+</style>
